22435529f934024b6691f36b066d8bfdafb3fbc1
abdulkadir18apr <114944605+abdulkadir18apr@users.noreply.github.com>
2023-02-27 12:33:55 +0530

    middle-ware

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,10 +7,11 @@ var bcrypt = require('bcryptjs')
 
 const { body, validationResult } = require('express-validator');
 var jwt = require('jsonwebtoken');
+const fetchuser=require('../middleware/fetchuser');
 
 const JWT_SECRET="AbdulkadirIsAGood$boy";
 
-//Create a user using POST request "/api/createuser/". No Login Required
+//ROUTE-1:  Create a user using POST request "/api/createuser/". No Login Required
 
 router.post('/createuser', [
     body('name', "Enter a Valid Name").isLength({ min: 3 }),
@@ -50,11 +51,59 @@ router.post('/createuser', [
     }
     catch (error) {
         console.log(error.message)
-        res.status(404).json("something Wen Wrong")
+        res.status(500).json("internal Serval error")
     }
 
     //check whether email exist already
 
+})
+// ROUTE-2: for  login of a user....................
+//No login Required
+router.post('/login',[body('email', "Enter a Valid email").isEmail(),body('password','password cannot bhi blank').exists()],async (req,res)=>{
+    const errors = validationResult(req);
+    //if error return Bad request and errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password}=req.body;
+    try{
+        let user=await User.findOne({email:email});
+        if(!user){
+            return res.status(400).json({error:"try login with correct credential"});
+        }
+        const passwordCompare=await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"try login with correct credential"});
+        }
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken=jwt.sign(data,JWT_SECRET);
+        res.json({authToken});
+
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(500).json("internal Serval error")
+    }
+
+})
+
+//ROUTE-3 Get Login User Details.............
+// Login Reurired
+router.post('/getuser',fetchuser,async (req,res)=>{
+
+    try{
+        userId=req.user.id;
+        const user=await User.findById(userId).select("-password");
+        res.json(user);
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(500).json("internal Serval error")
+    }
 })
 
 module.exports = router
